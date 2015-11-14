163adb70fc4f90670ea2f630095f2ee7e9ba7b80
Ente <ducksource@duckpond.ch>
2015-11-14 13:39:21 +0100

    catnet added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Catnet
-I CAN HAZ INTERNET?
+# catnet
+_I CAN HAZ INTERNET?_
+
+## Getting started
+
+1. Install WWW::Mechanize
+
+    ```sh
+        $ sudo cpan WWW::Mechanize
+    ```
+
+2. Add the following line to your squid.conf
+
+    ```
+        url_rewrite_program /path/to/catnet/catnet.pl
+    ```
+
+3. Either configure squid as http-proxy or iptables for transparent proxying
+4. Done! Enjoy the true interwebs
---
+++ catnet.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use WWW::Mechanize;
+
+$mech = WWW::Mechanize->new();
+$mech->stack_depth(0);
+
+$|=1;
+$count = 0;
+$pid = $$;
+while (<>) {
+        chomp;
+        my @X = split;
+        my $url = $X[0];
+        if( $url =~ /(.+\.(jpg|png|gif))/i ){
+            my $img = $1;
+            my $type = $2;
+            my $caturl = "http://thecatapi.com/api/images/get?format=src&size=small&type=$type";
+            my $resp = $mech->simple_request( HTTP::Request->new(GET => "$caturl") );
+            if( $resp->is_redirect ) {
+                my $location = $resp->header( "Location" );
+                my $uri = new URI( $location );
+                print "OK rewrite-url=\"$uri\"\n";
+            }else{
+                print "OK\n";
+            }
+        }else{
+            print "OK\n";
+        }
+        $count++;
+}
